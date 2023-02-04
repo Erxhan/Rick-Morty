bc20051cadc668d7ca90da7371c54b610b596fc8
Erxhan Selmani <erxhanselmani@icloud.com>
2023-02-04 23:49:23 +0100

    Add model types

---
+++ Rick&Morty/Models/RMCharacterStatus.swift
@@ -0,0 +1,14 @@
+//
+//  RMCharacterStatus.swift
+//  Rick&Morty
+//
+//  Created by Erxhan Selmani on 04/02/2023.
+//
+
+import Foundation
+
+enum RMCharacterStatus: String, Codable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case `unknown` = "unknown"
+}
---
+++ Rick&Morty/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  Rick&Morty
+//
+//  Created by Erxhan Selmani on 04/02/2023.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
