3e880d1c41bdcf705a132e868873d88fa3312721
Erxhan Selmani <erxhanselmani@icloud.com>
2023-02-04 23:30:14 +0100

    Add documentation

---
+++ Rick&Morty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  Rick&Morty
+//
+//  Created by Erxhan Selmani on 04/02/2023.
+//
+
+import Foundation
+
+/// Object that represents a single API call
+final class RMRequest {
+    
+}
